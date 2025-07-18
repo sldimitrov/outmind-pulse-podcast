107448e7598a478b55164b65b07000dc33258a6e
Slavi FE <slavidimitrov54@gmail.com>
2025-07-18 13:25:07 +0300

    feat(clips-fe) include axios in manageClips service

--- src/services/manageClips.js
+++ src/services/manageClips.js
@@ -1,12 +1,12 @@
+import axios from "axios";
+
 export default async function createClip(clipData) {
-  return fetch('https://outmind-pulse-podcast.onrender.com/clip', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(clipData)
-  }).then((res) => {
-    if (!res.ok) throw new Error('Clip creation failed')
-    return res.json()
-  })
+  return axios.post('https://outmind-pulse-podcast.onrender.com/clip', clipData)
+    .then(function (response) {
+      console.log('Clip created successfully:', response.data);
+      return response.data;
+    }).catch(function (error) {
+      console.log('Error creating clip:', error);
+      throw new Error('Failed to create clip');
+    })
 }
